ef5ee706294792cf6f8f815e041edd8ecf45a4b2
Akardian <mike.wuestenberg@googlemail.com>
2017-11-25 14:13:19 +0100

    Logig-Prolog-A1

---
+++ Praktikum Logik_Prolog/Aufgabe-1-Stammbaum.pl
@@ -0,0 +1,45 @@
+% Autor: Mike Wustenberg
+% Datum: 17.11.2017
+
+
+% Mannlich
+maennlich(peter).
+maennlich(hans).
+
+maennlich(vaterRaika).
+maennlich(karl).
+
+maennlich(manHana).
+maennlich(sohnHana).
+
+% Weiblich
+weiblich(sofie).
+weiblich(hana).
+
+weiblich(mutterKarl).
+weiblich(raika).
+
+weiblich(tochterHana).
+
+% Eltern
+elternteile(hans, peter).
+elternteile(hans, sofie).
+
+elternteile(hana, peter).
+elternteile(hana, sofie).
+
+elternteile(raika, sofie).
+elternteile(raika, vaterRaika).
+
+elternteile(karl, peter).
+elternteile(karl, mutterKarl).
+
+elternteile(sohnHana, hana).
+elternteile(sohnHana, manHana).
+
+elternteile(tochterHana, hana).
+elternteile(tochterHana, manHana).
+
+
+% Verheiratet
+verheiratet(peter, sofie).
